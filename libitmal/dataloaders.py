47e503fdf3103addffd645e59ca03f80cee01b8e
Bjarke Hansen <2takker@gmail.com>
2019-02-22 09:40:57 +0100

    Added dataloader module

---
+++ libitmal/dataloaders.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_openml
+from sklearn.datasets import load_iris
+
+
+def MOON_GetDataSet(n_samples = 100, shuffle = True, noise = None,
+                   random_state = None):
+    return make_moons(n_samples = n_samples, shuffle = shuffle, 
+                      noise = noise, random_state = random_state)
+
+   
+def MOON_Plot(X, y, title = "", xlabel = "", ylabel = ""):
+    plt.scatter(X[:,0], X[:,1], c = y)   
+    plt.title(title)
+    
+    plt.show()
+
+
+def MNIST_GetDataSet():
+    mnist = fetch_openml('mnist_784', version = 1, cache = True)
+    return mnist.data, mnist.target
+
+
+def MNIST_PlotDigit(data):
+    image = data.reshape(28, 28)
+    plt.imshow(image, cmap = 'Greys')
+
+
+def IRIS_GetDataSet(return_X_y = False):
+    return load_iris(return_X_y = return_X_y)
