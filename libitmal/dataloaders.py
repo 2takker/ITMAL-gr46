07e06368fc193cdc066454e27e78d1095fd07b4f
RasEza <rasmus.appel@gmail.com>
2019-02-25 12:31:37 +0100

    Push mine opgaver indtil videre

--- libitmal/dataloaders.py
+++ libitmal/dataloaders.py
@@ -1,33 +1,55 @@
+import sklearn 
+#%matplotlib inline
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import make_moons
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import fetch_openml
-from sklearn.datasets import load_iris
 
+def MOON_GetDataSet(n_samples = 100,noise = 0):
+    # TODO: your code here...
+    return sklearn.datasets.make_moons(n_samples=n_samples, noise=noise)
 
-def MOON_GetDataSet(n_samples = 100, shuffle = True, noise = None,
-                   random_state = None):
-    return make_moons(n_samples = n_samples, shuffle = shuffle, 
-                      noise = noise, random_state = random_state)
-
-   
-def MOON_Plot(X, y, title = "", xlabel = "", ylabel = ""):
-    plt.scatter(X[:,0], X[:,1], c = y)   
+def MOON_Plot(X, y,title="my title", xlable="", ylabel=""):
+    # TODO: some simple plot commands, 
+    # To plot pretty figures
+    colors = ['#FF0000' if x == 1 else '#0000ff' for x in y]
     plt.title(title)
-    
+    plt.xlabel = xlable
+    plt.ylabel = ylabel
+    plt.scatter(X[:,0],X[:,1],s = 1,color=colors)
     plt.show()
 
-
-def MNIST_GetDataSet():
-    mnist = fetch_openml('mnist_784', version = 1, cache = True)
-    return mnist.data, mnist.target
-
+from sklearn.datasets import fetch_mldata
+from tensorflow.keras.datasets import mnist
 
 def MNIST_PlotDigit(data):
     image = data.reshape(28, 28)
-    plt.imshow(image, cmap = 'Greys')
+    # TODO: add plot functionality for a single digit...
+    plt.imshow(image,cmap=matplotlib.cm.binary,interpolation="nearest")
+    plt.axis("off")
 
+def MNIST_GetDataSet():
+    # TODO: use mnist = fetch_mldata('MNIST original') or mnist.load_data(),
+    #       but return as a single X-y tuple     
+   
+    (trainx,trainy), (testx,testy) = mnist.load_data()
+    
+    xset = np.concatenate((trainx, testx))
+    yset = np.concatenate((trainy,testy))
+    
+    return xset, yset
 
-def IRIS_GetDataSet(return_X_y = False):
-    return load_iris(return_X_y = return_X_y)
+def IRIS_GetDataSet():
+	return sklearn.datasets.load_iris()
+
+def IrisFeatureComparePlot(data,feature_nOne=0,feature_nTwo=0):
+	#prepare colors
+    for x in data.target:
+        if x == 0:
+             colors.append('DarkBlue')
+        elif x == 1:
+             colors.append('DarkRed')
+        elif x == 2:
+             colors.append('Green')
+
+    plt.scatter(data.data[:,feature_nOne],data.data[:,feature_nTwo],s=1,color=colors)
